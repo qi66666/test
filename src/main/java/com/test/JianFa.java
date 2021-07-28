2ab777f22db7128b7be2c2a4dd17476f20510fb9
qi66666 <1835089810@qq.com>
2021-07-28 18:56:08 +0800

    第一次上传

---
+++ src/main/java/com/test/JianFa.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @Author：QiXiaoShuang
+ * @Classname：Jianfa
+ * @Date：2021/07/28——16:58
+ */
+public class JianFa {
+    public static void main(String[] args) {
+        System.out.println("第一次上传");
+    }
+}
